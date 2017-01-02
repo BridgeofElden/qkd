2e85e9d31116dbfac4b99439b241ae1b225885bb
Larry Davis <eldendavis44@gmail.com>
2017-01-02 12:51:35 -0600

    Completed revising match_qbits function

--- key_encryption.py
+++ key_encryption.py
@@ -5,9 +5,10 @@ Created on Fri Nov 25 21:46:54 2016
 
 @author: larry
 """
-from random import random, choice, shuffle
+from random import random, choice, randint
 import numpy as np
 import time
+from math import ceil
 # from Crypto.Cipher import AES
         
 class QKD():
@@ -62,9 +63,11 @@ def apply_identity_gate(bit):
     encoding and decoding methods are the same
     """
     if bit == 0:
-        return 0
+        pass
+#        return 0
     elif bit == 1:
-        return 1
+        pass
+#        return 1
     else:
         return "?"
         
@@ -77,128 +80,128 @@ def create_key(number_of_bits=8):
     # Create the key
     possible_bits = [1, 0]
     random_key = [choice(possible_bits) for i in range(number_of_bits)]
-    print(random_key)
+#    print(random_key)
     number_of_bits = int(number_of_bits)
     return random_key, string_to_be_encrypted, number_of_bits
         
     
     
-def apply_quantum_gates(number_of_bits, random_key):
+def apply_quantum_gates_alice(random_key):
     """
     Applies either Hadamard or identity quantum gate to each bit in number_of_bits.
     """
 
     # Create Qubits to represent the key
     # Apply Hadamard and identity gates randomly
-    alice_gate_list = []
     alice_qbit_list = []
-    bob_gate_list = []
-    bob_key_recovered = []
     probabilities = []
     possible_gates = ['hadamard', 'identity']
-    for i in range(number_of_bits):
+    for i in (random_key):
         qbit = QKD()
-        qbit_value = 0
         gate_chosen = choice(possible_gates)
         
         #applying Hadamard Gate
         if gate_chosen == 'hadamard':
-            qbit_value = apply_hadamard_gate(random_key[i], mode = "encoding")
-            alice_gate_list.append("{0}H".format(i + 1))
+            qbit_value = apply_hadamard_gate(i, mode = "encoding")
                 
         else :
             #applying identity gate
             qbit_value = apply_identity_gate(random_key[i])
-            alice_gate_list.append("{0}I".format(i + 1))
             
         probabilities.append((qbit.alpha, qbit.beta))
         alice_qbit_list.append(qbit_value)
         
-#        print('gate: {0}; qubit: {1}'.format(gate_chosen, str(qbit)))
+    print(random_key)
+    print(alice_qbit_list)        
+    return alice_qbit_list, possible_gates
         
-    alice_gate_tuple_1 = (alice_gate_list)
+def apply_quantum_gates_bob(alice_qbit_list, possible_gates):
     
-    for i in range(number_of_bits):
-        cbit_value = 0
+    bob_bit_list = []    
+    for i in (alice_qbit_list):
         gate_chosen = choice(possible_gates)
+        
         
         #applying Hadamard Gate
         if gate_chosen == 'hadamard':
-            cbit_value = apply_hadamard_gate(alice_qbit_list[i], mode = "decoding")
-            bob_gate_list.append("{0}H".format(i + 1))
+            bit_value = apply_hadamard_gate(i, mode = "decoding")
                 
         else :
             #applying identity gate
-            cbit_value = apply_identity_gate(alice_qbit_list[i])
-            bob_gate_list.append("{0}I".format(i + 1))
-            
-        bob_key_recovered.append(cbit_value)
-
-    
-#        print('gate: {0}'.format(gate_chosen))
-    
-    gate_matches = [i if i == j else "No Match" for i, j in zip(bob_gate_list, alice_gate_list)]
-    return gate_matches, bob_key_recovered, alice_qbit_list, alice_gate_list, bob_gate_list
-
-            
-def match_qbits(gate_matches, alice_qbit_list, alice_gate_list, bob_key_recovered, bob_gate_list, random_key, print_gates=True):
-    """
-    Finds qubits that correspond to gate matches and matches a selection of those qubits.
-    """        
-    
-    if gate_matches == []:
-        print("Whoops! The random gate generators did not produce any gates in common!")
-        print(gate_matches)
-        # using counters bc list indexes start at 0
-    else :
-        # finds the corresponding qubits to the gate matches
-        alice_qbit_nonint_matches = []
-        for gate_index, gate_string in enumerate(gate_matches):
-            if "H" in gate_string or "I" in gate_string:
-                alice_qbit_nonint_matches.append(str(gate_index))
-            else:
-                continue
-            
-        # chooses Alice's qubits to be disclosed
-        # checks that the choice generated 2 different numbers
-        alice_qbit_matches = [int(x) for x in alice_qbit_nonint_matches]
-        len_of_qbit_matches = len(alice_qbit_matches)
-        sample_number = len_of_qbit_matches / 2
+            bit_value = apply_identity_gate(i)
         
-        # Chooses qubits here
-        # also checks equality
-        list_of_chosen_matches = [i for i in range(len_of_qbit_matches)]
-        shuffle(list_of_chosen_matches)
-        # list_of_chosen_matches = shuffle(list_of_chosen_matches)
-        disclosed_qbits = [alice_qbit_matches[list_of_chosen_matches[i]] for i in range(sample_number)]
-                    
-        #creates lists of actual qubits and matches them
-        bob_value_list = [bob_key_recovered[x] for x in disclosed_qbits]
-        alice_value_list = [random_key[x] for x in disclosed_qbits]
-        qbit_matches = [i for i, j in zip(bob_value_list, alice_value_list) if i == j]
-        if len(qbit_matches) < len(bob_value_list):
-            raise Exception("Communications error! Security is possibly compromised!")
+        bob_bit_list.append(bit_value)
+                            
+    print(bob_bit_list)
+        
+    return bob_bit_list
+            
+
+
+            
+def match_qbits(random_key, bob_bit_list):
+    """
+    Matches lists of bits. Taken from original list and bob's decoded list.
+    """
+    #choosing bits to compare
+#    number_of_failed_conversions = bob_bit_list.count("?")
+#    print(len(bob_bit_list))
+    new_key = []
+    alice_success_list = []
+    bob_success_list = []            
+        
+    for i in range(0, 63):
+        alice_bit = random_key[i]
+        bob_bit = bob_bit_list[i]
+        if bob_bit == "?":
+            continue
         else:
-            print('Lists match -- Key is secure!!\nKey: {0}'.format(qbit_matches))
+            alice_success_list.append(alice_bit)
+            bob_success_list.append(bob_bit)
+            
+            
+    bob_success_list_len = len(bob_success_list)
+    number_of_iterations = int(ceil(bob_success_list_len/2))
+    print(number_of_iterations)
+        
+    for i in range(0, number_of_iterations):
+        alice_success_bit = alice_success_list[i]
+        bob_success_bit = bob_success_list[i]
 
-    if print_gates == True:
-        print(random_key)
-        print(alice_gate_list)
-        print(bob_gate_list)
-        print(gate_matches)
-        print(alice_qbit_matches)
-        print(disclosed_qbits)
-        print('bob list = {0}'.format(bob_value_list))
-        print('alice list = {0}'.format(alice_value_list))
-        print('matches: {0}'.format(qbit_matches))
 
-    return qbit_matches
+        print(alice_success_bit)
+        print(bob_success_bit)
+        
+        
+        if alice_success_bit == bob_success_bit:
+            continue
+            print("-" + i)
+            
+            
+        else:
+            raise Exception ('Eavesdropper detected, aborting communications')
+            quit
+            
+    print(bob_success_list)        
+    del bob_success_list[0:number_of_iterations]
+    new_key = bob_success_list
+    print("hello")    
+    print(new_key)        
+    #not to be printed in final code version
+        
+        
+
+
+
+
+    return new_key
 """
 EXPERIMENTAL
+"""
 def eve_interference(alice_qbit_list):
-    """"""
+    """
     Runs simulated interference from Eve, the third party in BB84
-    """"""
+    """
     for n in range(len(alice_qbit_list)):
         print(alice_qbit_list)
         alice_qbit = alice_qbit_list[n]
@@ -216,7 +219,7 @@ def eve_interference(alice_qbit_list):
             apply_identity_gate(alice_qbit)
     print(alice_qbit_list[n])    
     return alice_qbit_list
-"""
+
 
 def aes_encryption(key):
     """
@@ -228,33 +231,61 @@ def aes_encryption(key):
             new_bit = choice([1, 0])
             key.format(new_bit)
             
-        
+time_list_1 = []
+time_list_2 = []
+time_list_3 = [] 
 
-def run_main():
-
+def run_main(time_list_1, time_list_2, time_list_3): 
     time1 = time.time()
-    random_key, string_to_be_encrypted, number_of_bits = create_key(number_of_bits=32)
+    random_key, string_to_be_encrypted, number_of_bits = create_key(number_of_bits=128)
     time2 = time.time()
     print("random_key_runtime: {0} seconds.".format(time2 - time1))
-    gate_matches, bob_key_recovered, alice_qbit_list, alice_gate_list, bob_gate_list = apply_quantum_gates(number_of_bits, random_key)
+    alice_qbit_list, possible_gates = apply_quantum_gates_alice(random_key)
+    bob_bit_list = apply_quantum_gates_bob(alice_qbit_list, possible_gates)
     time3 = time.time()
     print("apply_quantum_gates_runtime: {0} seconds.".format(time3 - time2))
+    match_qbits(random_key, bob_bit_list)
     #    alice_qbit_list = eve_interference(alice_qbit_list)
-    qbit_matches = match_qbits(gate_matches, alice_qbit_list, alice_gate_list, bob_key_recovered, bob_gate_list, random_key, print_gates=False)
-    print("Runtime: {0} seconds.".format(time.time() - time3))
     
-# Create the key for Alice
-    # Make a random list of quantum gates Alice
-    # Apply the gates to each key bit to create the quantum qubit states
-    # Send the qubits to Bob
-    # Make a random list of quantum gates for Bob
-    # Apply the gates to qubits received from Alice
-    # Get the set of matching gates between Alice and Bob
-    # From the matching gates choose half and the bit values for Alice and Bob
-    # If the bit values match, use the remaining bits from matching gates as a key
-    # Use the key to Encrypt the data using AES        
+    time4 = time.time()
+    print("Runtime: {0} seconds.".format(time.time() - time1))
+    time_list_1.append(time2 - time1)
+    time_list_2.append(time3 - time2)
+    time_list_3.append(time4 - time1)
+    return time_list_1, time_list_2, time_list_3
+    """
+    Alice makes 128 bit string
+    Alice encodes into qubits
+    Alice transmits qubits to Bob
+    Bob receives qubits
+    Bob decodes qubits
+    Compare bit strings
+    If equal, secure communication
+    Use remaining undisclosed qubits as key
+    If unequal, unsecure communication (abort)
+    
+    Alice makes 128 bit string
+    Alice encodes into qubits
+    Alice transmits qubits to Bob
+    Eve receives qubits
+    Eve decodes qubits
+    Eve reencodes qubits
+    Eve transmits to Bob
+    Bob receives qubits
+    Bob decodes qubits
+    Compare bit strings
+    If equal, secure communication
+    Use remaining undisclosed qubits as key
+    If unequal, unsecure communication (abort)
+    """      
 
 
 if __name__ == '__main__':
-    run_main()
+    run_main(time_list_1, time_list_2, time_list_3)
+
+    
+    
+    
+    
+
     
